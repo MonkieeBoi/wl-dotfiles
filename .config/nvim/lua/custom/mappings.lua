717c4ebd31c0528b9421192aea3a646f3df29b2f
MonkieeBoi <danny.pham26425@gmail.com>
2023-09-10 23:00:02 +1000

    restore default behavior and small fix

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -3,8 +3,18 @@ local M = {}
 
 M.general = {
   v = {
-    ["<S-Tab>"] = {"<gv", "unindent line"},
-    ["<Tab>"] = {">gv", "indent line"},
+    ["<S-Tab>"] = { "<gv", "unindent line" },
+    ["<Tab>"] = { ">gv", "indent line" },
+  },
+  n = {
+    ["j"] = { "j", "Move down" },
+    ["k"] = { "k", "Move up" },
+  },
+  x = {
+    ["j"] = { "j", "Move down" },
+    ["k"] = { "k", "Move up" },
+    ["p"] = { 'p:let @*=@0<CR>:let @"=@0<CR>', "Dont copy replaced text", opts = { silent = true } },
+
   }
 }
 
