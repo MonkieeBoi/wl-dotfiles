d9594d0107b05d9c7782ce45c166de9a3ed4f6cf
MonkieeBoi <danny.pham26425@gmail.com>
2025-03-03 22:11:33 +1100

    fix bleeding youtube style

--- .config/qutebrowser/stylemap.py
+++ .config/qutebrowser/stylemap.py
@@ -1,6 +1,6 @@
 styles = {
     'github.com.css': ['https://github.com/*'],
     'jezevec10.com.css': ['https://*.jezevec10.com/*'],
-    'youtube.com.css': ['https://*.youtube.com/*'],
+    'youtube.com.css': ['https://www.youtube.com/*'],
     'jpdb.io.css': ['https://jpdb.io/*'],
 }
