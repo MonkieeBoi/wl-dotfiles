11d4e2829946e95f036b8e63d349ae3428b08cfa
MonkieeBoi <danny.pham26425@gmail.com>
2025-05-02 02:42:44 +1000

    update qutebrower config

--- .config/qutebrowser/stylemap.py
+++ .config/qutebrowser/stylemap.py
@@ -3,4 +3,5 @@ styles = {
     'jezevec10.com.css': ['https://*.jezevec10.com/*'],
     'youtube.com.css': ['https://www.youtube.com/*'],
     'jpdb.io.css': ['https://jpdb.io/*'],
+    'fumenities.css': ['https://hsterts.github.io/Fumenities/'],
 }
